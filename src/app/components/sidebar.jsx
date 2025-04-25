2612a84dbfb94839de2852fc8b9f963870a7117e
Lena Ertl <lertl@hm.edu>
2025-04-25 17:35:51 +0200

    sidebar icons added

--- src/app/components/sidebar.jsx
+++ src/app/components/sidebar.jsx
@@ -1,5 +1,6 @@
 {/* Sidebar-Komponente basiert auf Standard-Seitenleiste von Flowbite (https://flowbite.com/docs/components/sidebar/) */ }
 import { useState } from "react";
+import Image from "next/image";
 
 export default function Sidebar({ onToggle }) {
     const [collapsed, setCollapsed] = useState(false);
@@ -21,19 +22,18 @@ export default function Sidebar({ onToggle }) {
                         <a
                             href="#"
                             style={{ color: "var(--foreground)" }}
-                            className="flex items-center p-4 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                            className="flex items-center p-4 text-gray-900 rounded-lg dark:text-white hover:bg-[var(--background)] dark:hover:bg-[var(--background)] group"
                             onClick={toggleSidebar}
                         >
-                            <svg
-                                className="w-5 h-5 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                                aria-hidden="true"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="currentColor"
-                                viewBox="0 0 22 21"
-                            >
-                                <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
-                                <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
-                            </svg>
+                            <Image
+                                className="full"
+                                src="./nobody_avatar.svg"
+                                alt="Avatar"
+                                width={30}
+                                height={30}
+                                style={{ width: "30px", height: "30px", marginRight: "10px", fill: "var(--foreground)" }}
+                            />
+
                             <span className={`ms-3 ${collapsed ? 'hidden' : ''}`}>Profil</span>
                         </a>
                     </li>
@@ -43,18 +43,17 @@ export default function Sidebar({ onToggle }) {
                         <a
                             href="#"
                             style={{ color: "var(--foreground)" }}
-                            className="flex items-center p-4 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                            className="flex items-center p-4 text-gray-900 rounded-lg dark:text-white hover:bg-[var(--background)] dark:hover:bg-[var(--background)] group"
                             onClick={toggleSidebar}
                         >
-                            <svg
-                                className="shrink-0 w-5 h-5 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                                aria-hidden="true"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="currentColor"
-                                viewBox="0 0 18 18"
-                            >
-                                <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z" />
-                            </svg>
+                            <Image
+                                className="full"
+                                src="./leaderboard.svg"
+                                alt="Leaderboard"
+                                width={25}
+                                height={25}
+                                style={{ width: "25px", height: "25px", marginRight: "10px", fill: "var(--foreground)" }}
+                            />
                             <span className={`flex-1 ms-3 whitespace-nowrap ${collapsed ? 'hidden' : ''}`}>Leaderboard</span>
                         </a>
                     </li>
